c538729c114b6da3e29a0c70123b83883b892de0
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-04-18 11:59:47 -0400

    Refactor setState

--- src/Counter.js
+++ src/Counter.js
@@ -11,14 +11,22 @@ class Counter extends Component {
     return (
       <div>
         <h2>{this.state.counter}</h2>
-        <button onClick={() => this.setState((state) => ({
-          counter: state.counter + 1,
-        }))}>
+        <button onClick={() => (
+            this.setState(
+              (state) => (
+                {counter: state.counter + 1,}
+              )
+            )
+        )}>
           +
         </button>
-        <button onClick={() => this.setState((state) => ({
-          counter: state.counter - 1,
-        }))}>
+        <button onClick={() => (
+            this.setState(
+              (state) => (
+                {counter: state.counter - 1,}
+              )
+            )
+        )}>
           -
         </button>
       </div>
