58c970188771113548303500f7dc318cea065c2c
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-04-18 11:09:24 -0400

    Hello World as nested components

---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+  render() {
+    return (
+      <React.Fragment>Hello</React.Fragment>
+    );
+  }
+}
+
+export default Hello
---
+++ src/World.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class World extends Component {
+  render() {
+    return (
+      <React.Fragment>World</React.Fragment>
+    );
+  }
+}
+
+export default World
