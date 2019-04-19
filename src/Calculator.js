c4e879dd86a9cfc6ffcd5ad12cf2125b0d97fbc1
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-04-19 11:22:35 -0400

    Implement NumberButton

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
 
+function NumberButton(props) {
+  return <button>{props.val}</button>;
+}
+
 class Calculator extends Component {
   renderNumbers (i) {
     const numbers = [0, 1, 2, 3];
-    const numberButtons = numbers.map((number) =>
-      <button key={number}>{number}</button>
+    const tests = numbers.map(
+      (number) => <NumberButton val={number} />
     );
-    return numberButtons
+    return tests
   }
   render () {
     return (
       <div>
-      <h3>Calculator</h3>
-      {this.renderNumbers(0)}
+        <h3>Calculator</h3>
+        {this.renderNumbers()}
       </div>
     );
   }
